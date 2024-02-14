48a5575c433436c620badad2780e0022233513f6
Rodrigo Santos <rr.santos1232@gmail.com>
2024-02-14 14:33:09 +0000

    stuff

--- lua/rodsnts/set.lua
+++ lua/rodsnts/set.lua
@@ -13,12 +13,10 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
---vim.opt.shell = "powershell"
 
 vim.opt.termguicolors = true
 
